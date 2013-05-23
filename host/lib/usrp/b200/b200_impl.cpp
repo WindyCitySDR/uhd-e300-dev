9a11de87dbf742e2b4408655f4d4ca20b308ee65
Nicholas Corgan <nick.corgan@ettus.com>
2013-05-23 09:42:21 -0700

    b200: fixed MSVC timeval incompatibility

--- host/lib/usrp/b200/b200_impl.cpp
+++ host/lib/usrp/b200/b200_impl.cpp
@@ -35,6 +35,15 @@
 #include <ctime>
 #include <iostream>
 
+#ifdef _MSC_VER
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#endif
+
+#endif
+
 using namespace uhd;
 using namespace uhd::usrp;
 using namespace uhd::transport;
