51f4aa9bc3a66bc2138c172bf5b0df39bb278104
Nicholas Corgan <nick.corgan@ettus.com>
2014-01-17 10:59:00 -0800

    usrp_x3xx_fpga_burner: added comment about post-burning sleep

--- host/utils/usrp_x3xx_fpga_burner.cpp
+++ host/utils/usrp_x3xx_fpga_burner.cpp
@@ -485,7 +485,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
      * Attempt to find USRP after burning
      */
     std::cout << std::endl << "Attempting to find device..." << std::flush;
-    boost::this_thread::sleep(boost::posix_time::milliseconds(2000));
+    boost::this_thread::sleep(boost::posix_time::milliseconds(2000)); //Sometimes needed for Ethernet to reconnect
     device_addr_t found_usrp = (vm.count("addr")) ? find_usrp_with_ethernet(ip_addr, false)
                                                   : find_usrp_with_pcie(resource, false);
     std::cout << "found!" << std::endl; //If unsuccessful, runtime error would occur in find functions
