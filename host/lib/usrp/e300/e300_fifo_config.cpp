a0c7041b725d08d44e3b18d55f5cf006d9167fda
Moritz Fischer <moritz@ettus.com>
2014-06-25 13:29:56 -0700

    e300: global_regs: get_global_regs_base() should not be private.
    Signed-off-by: Moritz Fischer <moritz@ettus.com>

--- host/lib/usrp/e300/e300_fifo_config.cpp
+++ host/lib/usrp/e300/e300_fifo_config.cpp
@@ -357,6 +357,11 @@ public:
         return this->_make_xport(which_stream, args, false);
     }
 
+    size_t get_global_regs_base() const
+    {
+        return REG_BASE(_ctrl_space);
+    }
+
 private:
     uhd::transport::zero_copy_if::sptr _make_xport(const size_t which_stream, const uhd::device_addr_t &args, const bool is_recv)
     {
@@ -391,11 +396,6 @@ private:
         }
 
         return xport;
-    }
-
-    size_t get_global_regs_base() const
-    {
-        return REG_BASE(_ctrl_space);
     }
 
     e300_fifo_config_t _config;
